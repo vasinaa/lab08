e174e3ce4e2d10a507f8e90782077cbcc27b8fd5
vasinaa <99878647+vasinaa@users.noreply.github.com>
2022-06-02 20:17:36 +0300

    Add files via upload

---
+++ hello_world_application/_build/CMakeFiles/lab03.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab03.dir/hello_world.cpp.o"
+  "CMakeFiles/lab03.dir/hello_world.cpp.o.d"
+  "lab03"
+  "lab03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
